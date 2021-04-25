d67278345f0e6b2533810b5a938a24cc349c9025
TokuyamaR <qpflapper.1007@gmail.com>
2021-04-25 09:17:39 +0900

    fetch api data

---
+++ src/features/fetch/Fetch.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch, useStore } from "react-redux";
+import { fetchAsyncGet, selectUsers } from "./fetchSlice";
+
+const Fetch = () => {
+  const dispatch = useDispatch();
+  const users = useSelector(selectUsers);
+  useEffect(() => {
+    // useEffectにてdispatchを指定した場合は、第二引数にdispatchを指定
+    dispatch(fetchAsyncGet());
+  }, [dispatch]);
+  return (
+    <div>
+      {users.map((user) => (
+        <div key={user.id}>{user.email}</div>
+      ))}
+    </div>
+  );
+};
+
+export default Fetch;
